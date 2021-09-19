e6d0d727cec4e4121195112d6489bb01cbcf6fda
shawijn <shabier@buildinamsterdam.com>
2021-09-19 11:40:37 +0200

    Inital

---
+++ api/article.js
@@ -0,0 +1,8 @@
+export default async (req, res) => {
+  let word  = req.query.word
+  let article = await fetch(`https://www.welklidwoord.nl/${word}`)
+    .then(res => res.text())
+    .then(data => data.match(/<span test>(.*)<\/span>/)[1])
+    .catch(err => err)
+  return res.status(200).json({aricle: article === 'De of Het' ? 'Helaas, we zijn nog niet zo slim is het wel een zelfstandig naamwoord?' : article})
+}
